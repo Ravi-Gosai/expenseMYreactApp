55c7c375fc9598e799159fc1000ba3ee1504b78a
Ravi Gosai <rbgosai86@gmail>
2024-02-18 12:53:07 +0530

    18-2-24 12:52 PM

--- src/App.js
+++ src/App.js
@@ -3,11 +3,50 @@ import './App.css';
 import ExpenseItem from './Component/ExpenseItem';
 
 function App() {
+  // const expends = {
+  //   id:1,
+  //   title:"car insurance !",
+  //   amount : 264.55,
+  //   date : new Date(2023,3,15),
+  //   expendLoc : 'moive 1'
+  // }
+
+  const expends = [
+    {
+      id:1,
+      title:"car insurance !",
+      amount : 264444.55,
+      date : new Date(2023,6,16),
+      expendLoc : 'manali'
+    },
+    {
+      id:2,
+      title:"bike insurance !",
+      amount : 264.55,
+      date : new Date(2023,4,15),
+      expendLoc : 'junagadh'
+    },
+    {
+      id:3,
+      title:"truck insurance !",
+      amount : 444.55,
+      date : new Date(2023,3,12),
+      expendLoc : 'mumbai'
+    },
+    {
+      id:4,
+      title:"cycle insurance !",
+      amount : 2644.55,
+      date : new Date(2023,3,15),
+      expendLoc : 'goa'
+    }
+  ]
   return (
    <div>
 
      <h1>let get strated</h1>
-     <ExpenseItem></ExpenseItem>
+     {expends.map(ele=><ExpenseItem {...ele} key={ele.id}></ExpenseItem>)}
+     {/* <ExpenseItem {...expends}></ExpenseItem> */}
    </div>
  
   );
--- src/Component/ExpenseItem.js
+++ src/Component/ExpenseItem.js
@@ -1,19 +1,19 @@
 import './ExpenseItem.css'
 
 
-function ExpenseItem(){
-    const expenseDate = new Date(2021,2,28);
-    const expenseTitle = 'Car Insurance !';
-    const expenseAmount = 264.55;
-    const locationOfExpenditure = 'moive'
+function ExpenseItem(props){
+    // const expenseDate = new Date(2021,2,28);
+    // const expenseTitle = 'Car Insurance';
+    // const expenseAmount = 264.55;
+    // const locationOfExpenditure = 'moive'
 
     return (
         <div className="expense-item"> 
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="xpense-item__pricee">{locationOfExpenditure}</div>
-                <div className="expense-item__price"> ${expenseAmount} </div>
+                <h2>{props.title}</h2>
+                <div className="LocOFExpend">{props.expendLoc}</div>
+                <div className="expense-item__price"> ${props.amount} </div>
             </div>
            
         </div>
