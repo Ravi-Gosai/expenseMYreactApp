ff0663908089a70aeba7ce7cb640c986ba52ea6b
Ravi Gosai <rbgosai86@gmail>
2024-03-11 11:23:28 +0530

    11-3-24 11.33

--- src/Component/Expenses/Expenses.js
+++ src/Component/Expenses/Expenses.js
@@ -7,21 +7,45 @@ import { useState } from 'react';
 const Expenses = (props)=>{
 
     const [filteredYear, setFilteredYear] = useState('2020');
-    const [filterItem, setFilterItem] = useState(props.items)
+    // const [filterItem, setFilterItem] = useState(props.items)
     const filterChangeHandler = selectedYear => {
       setFilteredYear(selectedYear);
-      setFilterItem(props.items.filter((ele) => ele.date.getFullYear() === +selectedYear))
-      console.log(filterItem)
+
+      // setFilterItem(props.items.filter((ele) => ele.date.getFullYear() === +selectedYear))
+      // console.log(filterItem)
     };
+
+    const filterItems = props.items.filter((expense)=> expense.date.getFullYear().toString() === filteredYear)
+
+    // aa method pn easy to use chhe niche aapde biji 2 mehtod joi chhe
+    let expenseContent = <p> no expenses found</p>;
+
+    if(filterItems.length > 0){
+      expenseContent = filterItems.map((ele) => (
+        <ExpenseItem {...ele} key={ele.id}></ExpenseItem>
+      ))
+    }
+
     return(
         <div>
             <Card className="card">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
 
         <div id="parDiv">
-          {filterItem.map((ele) => (
+          {/* {filterItems.length === 0 ? <p>no expenses found</p> : (filterItems.map((ele) => (
             <ExpenseItem {...ele} key={ele.id}></ExpenseItem>
-          ))}
+          )))} */}
+
+          {/* {//upar nu kam niche na mehotd thi andOperator thi agal ni condiion true hoy to and sign pachhi ni code execute thai } */}
+
+          {/* {filterItems.length === 0 && <p> no expense found</p>}
+          {filterItems.length > 0 && (filterItems.map((ele) => (
+            <ExpenseItem {...ele} key={ele.id}></ExpenseItem>
+          )))} */}
+
+          {expenseContent}
+          {filterItems.length === 1 && <p>Only single Expense here. Please add more...</p>}
+          
         </div>
         {/* <ExpenseItem {...expends}></ExpenseItem> */}
       </Card>
